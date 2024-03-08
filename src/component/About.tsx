8b90acddfc7a2f1ebb653584304e18991c4ad22f
Vladislav <www.vladtarna25@gmail.com>
2024-03-08 16:37:36 +0200

    aaaa

--- src/component/About.tsx
+++ src/component/About.tsx
@@ -8,7 +8,7 @@ type AboutProps = {
 const About: React.FC<AboutProps> = ({ title, text }) => {
   return (
     <div className="container mx-auto max-w-2xl text-center py-12 xl:py-28">
-      <h2 className="text-3xl font-bold text-green-900 md:text-5xl">{title}</h2>
+      <h2 className="text-3xl font-bold text-red-900 md:text-5xl">{title}</h2>
       <p className="mt-4 text-green-700">{text}</p>
     </div>
   );
