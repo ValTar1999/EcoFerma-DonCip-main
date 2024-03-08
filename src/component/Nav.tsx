eafd7cedae8a82a9842a682eca55a5a09bbf0c1f
Vladislav <www.vladtarna25@gmail.com>
2024-03-08 21:28:07 +0200

    up nav

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -9,8 +9,9 @@ const Nav: React.FC<NavProps> = ({}) => {
   return (
     <nav className="bg-green-900 fixed z-10 w-full p-2">
       <div className="container mx-auto max-w-6xl px-4">
-        <a href="#">
+        <a className="flex items-center gap-3" href="#">
           <img className="h-8 w-8" src={Logo} alt="" />
+          <div className="text-green-50 font-semibold text-lg">EcoFerma DonCip</div>
         </a>
       </div>
     </nav>
